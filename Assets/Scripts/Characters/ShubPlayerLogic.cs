abd475b938a0f61924ef8d9e68d77b6333067bda
elymne <sacha.djurdjevic@gmail.com>
2021-01-11 16:16:49 +0100

    [FEAT] structuring project.

---
+++ Assets/Scripts/Characters/ShubPlayerLogic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShubPlayerLogic : AbstractCharacterLogic
+{
+
+    public int PlayerStatus = Status.NONE;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Grounds/AbstactGroundLogic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstactGroundLogic : MonoBehaviour
+{
+    // Status information of ground element.
+    protected int status;
+
+    // Current nature effect of the ground.
+    protected int nature;
+
+    // Current object posed on the ground (can be item, door, trap ect...).
+    protected Object posedObject;
+
+    // Define what's ahappen when character touch this ground.
+    protected abstract void OnCharacterTouched(AbstractCharacterLogic character);
+
+}
